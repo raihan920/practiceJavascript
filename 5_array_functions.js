eb51f6096c2dfe3e485608d474d5d3d0939e2437
Raihan Chowdhury <raihan92@rocketmail.com>
2023-01-16 15:36:21 +0600

    using spread operator

--- 5_array_functions.js
+++ 5_array_functions.js
@@ -34,3 +34,26 @@ console.log(myArr);
 //array slice adding new element in array
 let newArr = ['It', ...myArr.slice(1)];     //...denotes flating an array(it is called 'spread operator')
 console.log(newArr);
+
+//use of 'spread operator'
+const arr1 = ['I', 'love', 'programming'];
+const arr2 = ['We', 'love', 'web', 'development'];
+const arr3 = [...arr1,'and',...arr2];
+console.log(arr3);
+
+//more use of 'spread operator'
+const studentInfo = {
+    student_id: 123,
+    studetn_name: "Abdur Rahman",
+    student_age: 30,
+    student_gender: "Male",
+    student_department: "CSE"
+}
+console.log(studentInfo);
+
+const updatedStudent = {
+    ...studentInfo,
+    student_session: "Spring-2022"
+}
+
+console.log(updatedStudent);
